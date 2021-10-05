e813461d243bfe376f9d86f59ad117e24d501eaf
Konstantin T <tconsta@users.noreply.github.com>
2021-10-05 17:55:31 +0700

    CsvInputHandler implemented

--- src/handlers.py
+++ src/handlers.py
@@ -11,27 +11,32 @@ class BaseHandler:
 
 class DataStructIdent:
     """Stores the received list of fields or generates it based on the file."""
-
     def __init__(self, file_path: str = None, fields: list = None) -> None:
         self.file_path = file_path
         self.fields = fields if fields else []
 
-    def get_fields_from_csv_heading(self, sep=','):
+    def get_fields_from_csv_head(self, sep=','):
         raise NotImplementedError
 
 
 class CsvInputHandler(BaseHandler):
+    """
+    Receives data from a CSV file.
+
+    Places it in the desired order, filtering out unnecessary fields.
+    """
     def __init__(self, file_path: str, fields: list, delimiter=',') -> None:
         self.delimiter = delimiter
         super().__init__(file_path, fields)
 
-    def get_data_row_gen(self):
+    def get_row_gen(self):
         with open(self.file_path, newline='') as csv_input:
-            reader = csv.DictReader(csv_input, fieldnames=self.fields,
-                                    delimiter=self.delimiter)
-            next(reader)  # skip heading
+            reader = csv.DictReader(csv_input, delimiter=self.delimiter)
+            # heading already grabbed by reader, start read data
             for row in reader:
-                yield row
+                # delete unnecessary data and order as required
+                nice_data = {key: row[key] for key in self.fields}
+                yield nice_data
 
 
 class XmlInputHandler(BaseHandler):
--- src/main.py
+++ src/main.py
@@ -11,10 +11,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 INPUT_DIR = os.path.join(BASE_DIR, 'data_input')
 OUTPUT_DIR = os.path.join(BASE_DIR, 'data_output')
 
-ds = DataStructIdent(fields=['D1', 'D2', 'D3', 'M1', 'M2', 'M3'])
+ds = DataStructIdent(fields=['D2', 'D1', 'D3', 'M1', 'M2', 'M3'])
 
 path1 = os.path.join(INPUT_DIR, 'csv_data_1.csv')
 src1 = CsvInputHandler(path1, ds.fields, delimiter=',')
 
-for row in src1.get_data_row_gen():
+for row in src1.get_row_gen():
     print(row)
