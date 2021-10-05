e9b0292b25e14bbc3778a345981a0e6198ead822
Konstantin T <tconsta@users.noreply.github.com>
2021-10-06 00:09:23 +0700

    added all data sources handling

--- src/main.py
+++ src/main.py
@@ -4,26 +4,50 @@
 
 import os
 from pathlib import Path
+import itertools
 
 from handlers import (DataStructIdent, CsvInputHandler,
                       XmlInputHandler, JsonInputHandler,
                       CsvWriter, DbFiller, DbQuery)
 
 BASE_DIR = Path(__file__).resolve().parent.parent
+# Extract data from
 INPUT_DIR = os.path.join(BASE_DIR, 'data_input')
+# Load data to
 OUTPUT_DIR = os.path.join(BASE_DIR, 'data_output')
 
+# Define required data fields and its order
 ds = DataStructIdent(fields=['D2', 'D1', 'D3', 'M1', 'M2', 'M3'])
 
+# Data source 1
 path1 = os.path.join(INPUT_DIR, 'csv_data_1.csv')
 src1 = CsvInputHandler(path1, ds.fields, delimiter=',')
 it1_from_csv1 = src1.get_row_gen()
 
+# Data source 2
+path2 = os.path.join(INPUT_DIR, 'csv_data_2.csv')
+src2 = CsvInputHandler(path2, ds.fields, delimiter=',')
+it2_from_csv2 = src2.get_row_gen()
 
+# Data source 3
+path3 = os.path.join(INPUT_DIR, 'json_data.json')
+src3 = JsonInputHandler(path3, ds.fields)
+it3_from_json = src3.get_row_gen()
+
+# Data source 4
+path4 = os.path.join(INPUT_DIR, 'xml_data.xml')
+src4 = XmlInputHandler(path4, ds.fields)
+it4_from_xml = src4.get_row_gen()
+
+# Combine all sources
+all_sources_it = itertools.chain(it1_from_csv1, it2_from_csv2,
+                                 it3_from_json, it4_from_xml)
+
+# Intermediate results: database
 db_path = os.path.join(OUTPUT_DIR, 'quite_a_few_Gb.sqlite3')
 db = DbFiller(db_path, ds.fields)
 db.create_table()
-db.write(it1_from_csv1)
+db.write(all_sources_it)
 
 sql_basic = 'SELECT * FROM "important_data" ORDER BY "D1"'
 sql_advanced = """SELECT D1, D2, D3, SUM(M1), SUM(M2), SUM(M3)
@@ -37,6 +61,7 @@ query_advanced = DbQuery(db_path, ds.fields, sql_advanced)
 it_basic = query_basic.get_row_gen()
 it_advanced = query_advanced.get_row_gen()
 
+# Final results
 path_basic = os.path.join(OUTPUT_DIR, 'basic_results.tsv')
 path_advanced = os.path.join(OUTPUT_DIR, 'advanced_results.tsv')
 
